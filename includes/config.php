0499dd5b5c2fd15ca35909170230c5e2ebb1a51b
rbn423 <33626150+rbn423@users.noreply.github.com>
2019-04-11 00:17:57 +0200

    Añadidas las funciones de usuario
    He añadido la clase Usuario, que contiene las funciones necesarias para el establecimiento de conexion con la tabla usuario y todo lo necesario para interactuar con ella, como buscar si un usuario existe en la base de datos, crearlo o actualizarlo.
    He modificado tambien el codigo relacionado con esta clase, como puede ser la vista registrado o procesar login, que ahora apenas tienen codigo dentro.

--- includes/config.php
+++ includes/config.php
@@ -1,18 +1,22 @@
 <?php 
 	require_once __DIR__.'/Aplicacion.php';
-	session_start();
+	
 
 	ini_set('default_charset', 'UTF-8');
 	setLocale(LC_ALL, 'es_ES.UTF.8');
 	date_default_timezone_set('Europe/Madrid');
 
+	if(!isset($_SESSION)) 
+    { 
 	/**
 	 * Parámetros de conexión a la BD
 	 */
-	define('BD_HOST', 'localhost');
-	define('BD_NAME', 'chesterplans');
-	define('BD_USER', 'admin');
-	define('BD_PASS', 'admin');
+		define('BD_HOST', 'localhost');
+		define('BD_NAME', 'chesterplans');
+		define('BD_USER', 'admin');
+		define('BD_PASS', 'admin');
+		session_start();
+	}	
 
 	// Inicializa la aplicación
 	$app = Aplicacion::getSingleton();
