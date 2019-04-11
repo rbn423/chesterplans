78c9a34b94018e973c7408c9ef796e4d9f69e99e
samusolo <samusolo@ucm.es>
2019-04-11 18:37:29 +0200

    Añadido fechas

--- vistaCrearViaje.php
+++ vistaCrearViaje.php
@@ -15,25 +15,23 @@
 		?>
 			<div id="contenido">
 				<?php
-					if(isset($_SESSION["login"])){ 
 						echo '<form method="post" action="viajeCreado.php">';
 							echo'<div id="escribirViaje">'; 
 								echo'<h3>Título:</h3>';
 								echo'<p><input type="text" name="titulo"/></p>';
+								echo'<h3>Fecha Inicio:</h3>';
+								echo'<p><input type="date" value="fechaini"/></p>';
+								echo'<h3>Fecha Fin:</h3>';
+								echo'<p><input type="date" value="fechafin"/></p>';
 								echo'<h3>Descripción breve:</h3>';
 								echo'<p><input type="text" name="descb" size="50"/></p>';
 								echo'<h3>Texto:</h3>';
 								echo'<p><textarea rows="10" cols="60" name="descg"></textarea></p>';
 								echo'<h3>Precio:</h3>';
-								echo'<p><textarea rows="1" cols="5" name="descg"></textarea></p>';
-								echo'<p><input type="submit" value="compartir"/></p>';
+								echo'<p><textarea rows="1" cols="5" name="precio"></textarea></p>';
+								echo'<p><input type="submit" value="Compartir"/></p>';
 							echo'</div>';
 						echo'</form>';
-					}
-					else{
-						echo '<h1>Usuario sin registrar</h1>' ;
-						echo '<p>No tienes permiso para crear viajes.</p>';			
-					}
 				?>
 			</div>
 		<?php
