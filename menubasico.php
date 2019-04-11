65c182c55605f1902187417a19f0977af404babb
aagudo01 <aagudo01@ucm.es>
2019-04-11 19:52:29 +0200

    Add files via upload
    menu basico sin link y estilo de css

--- menubasico.php
+++ menubasico.php
@@ -1,17 +1,17 @@
 <div id="menubasico">
 	<span id="apartado">
-		<a href="index.php">Exp.compartida</a>
+		<a href="">Exp.compartida</a>
 	</span>
 	<span id ="apartado">
-		<a href="vistaViajes.php">Puntos</a>
+		<a href="">Puntos</a>
 	</span>
 	<span id="apartado">
 		<a href="vistaCrearExperiencia.php">Crear Experiencia</a>
 	</span>
 	<span id ="apartado">
-		<a href="vistaExperiencias.php">Intereses guardados</a>
+		<a href="">Intereses guardados</a>
 	</span>
 	<span id ="ultimo">
-		<a href="vistaCrearExperiencia.php">historial</a>
+		<a href="">Historial</a>
 	</span>
 </div>
