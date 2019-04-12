109f785556787cac24d7b4bf09e24a40becaeed5
rbn423 <33626150+rbn423@users.noreply.github.com>
2019-04-12 20:18:58 +0200

    Cambios en los formularios
    He creado la clase FormularioRegistro, que se encarga de crear el código del formulario de registro. Creando esta clase nos evitamos tener un troncho de codigo en registro.php y ademas nos quitamos la vista registrado.
    IMPORTANTE!!! Mas ojo a la hora de hacer los commits, que alguien metio un sql nuevo sin mirar los cambios y pisaron el tamaño de la contraseña, por lo que nunca se podia iniciar sesion y me he vuelto loco hasta que he encontrado el error.

--- registro.php
+++ registro.php
@@ -1,5 +1,6 @@
 <?php
 	require("includes/config.php");
+	require("FormularioRegistro.php");
 ?>
 <html>
 	<head>
@@ -14,42 +15,10 @@
 			require('includes/comun/izquierda.php');
 		?>
 			<div id="contenido">
-				<form method="post" action="registrado.php">
-				<div id="registro">
-				<h1>Registro de Usuario</h1>
-				<p>
-					Nick de usuario:
-					<input type="text" name="usernombre"/>
-				</p>
-				<p>
-					Nombre: 
-					<input type="text" name="nombre"/>
-				</p>
-				<p>
-					Apellidos: 
-					<input type="text" name="apellidos"/>
-				</p>
-				<p>
-					Contraseña: 
-					<input type="password" name="contraseña"/>
-				</p>
-				<p>
-					Introduzca la contraseña de nuevo: 
-					<input type="password" name="rcontraseña"/>
-				</p>
-				<p>
-					Email: 
-					<input type="text" name="email"/>
-				</p>
-				<p>
-					Teléfono: 
-					<input type="text" name="telefono"/>
-				</p>
-				<p>
-					<input type="submit" value="Enviar">
-				</p>
-				</form>
-				</div>
+				<?php
+					$form = new FormularioRegistro();
+					$form->gestiona();
+				?>
 			</div>
 		<?php
 			require('includes/comun/derecha.php');
