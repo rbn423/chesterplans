d8472ca7ceee056bb39ba88689ba66adfa43f5b1
samusolo <samusolo@ucm.es>
2019-04-13 18:44:15 +0200

    Menu empresa

---
+++ menuempresa.php
@@ -0,0 +1,14 @@
+<div id="menuempresa">
+	<span id="apartado">
+		<a href="vistaCrearExperiencia.php">Crear Actividad</a>
+	</span>
+	<span id ="apartado">
+		<a href="vistaCrearViaje.php">Crear Viaje</a>
+	</span>
+	<span id="apartado">
+		<a href="vistaCrearCombo.php">Crear Combo</a>
+	</span>
+	<span id ="ultimo">
+		<a href="">Historial</a>
+	</span>
+</div>
