15ecb3803661dda8138241bc06bcfabeda523ccc
rbn423 <33626150+rbn423@users.noreply.github.com>
2019-04-14 19:35:22 +0200

    Vista combo completada y funcional

--- vistaCombo.php
+++ vistaCombo.php
@@ -3,42 +3,46 @@
 	$conn = $app->conexionBd();
 
 	function mostrarCombos($conn){
-		$sql = "SELECT id FROM combo";
+		$sql = "SELECT id, viaje, precio FROM combo";
 		$busquedas = $conn->query($sql);
 		$ncombos=$busquedas->num_rows;
 		$busquedas = $busquedas->fetch_all();
 		for ($i=0;$i<$ncombos;$i++){	
-			$valor = $busquedas[$i][0];
-			$sql = "SELECT idact FROM intercombo WHERE idcombo = '".$id."'";
-			$combo = $conn->query($sql);
-			$combo = $combo->fetch_all());
-			$nactividades=$combo->num_rows;
-			for($i=0;$i<nactividades;$i++){
-				$valor2 = $combo[$i][0];
-				$sql= "SELECT titulo FROM actividad WHERE id = '".$id."'";
+			$idcombo = $busquedas[$i][0];
+			$idViaje = $busquedas[$i][1];
+			$precio = $busquedas[$i][2];
+			$sql = "SELECT titulo FROM viaje WHERE id = '".$idViaje."'";
+			$tituloViaje = $conn->query($sql);
+			$tituloViaje = $tituloViaje->fetch_assoc();
+			$sql = "SELECT idact FROM intercombo WHERE idcombo = '".$idcombo."'";
+			$idActividades = $conn->query($sql);
+			$idActividades = $idActividades->fetch_all();
+			$nactividades=count($idActividades);
+			$html = "<ul>";
+			for($i=0;$i<$nactividades;$i++){
+				$sql= "SELECT titulo FROM actividad WHERE id = '".$idActividades[$i][0]."'";
+				$tituloAct = $conn->query($sql);
+				$tituloAct = $tituloAct->fetch_assoc();
+				$html .= "<li><h2>".$tituloAct['titulo']."</h2></li>";
 			}
 			if($i!=$ncombos-1){
 				echo '<div id="combo">';
 			}
-			else{
+			else
 				echo '<div id="ultimocombo">';
-				echo '<div id="info">';
-				echo '<h1>'.$viaje["TITULO"].'</h1>';
-				echo '<p>'.$viaje["DESCB"].'<p>';
-				echo '<p>De '.$viaje["FECHAINI"].' a '.$viaje["FECHAFIN"]. '<p>';
-				echo '<h2>'.$actividad["TITULO"].'</p>';
-				echo '<p>' .$actividad["DESCB"].'</p>';
-				echo '<p>Precio: '.$combo["PRECIO"].'</p>';
-				echo '</div>';
-				echo '<form method="post" action="viaje.php?id='.$valor.'">';
-				echo '<div id="boton">';
-				echo '<input type="submit" value="Ver mas">';
-				echo '</div>';
-				echo '</form>';
-				echo '</div>';
+			echo '<div id="info">';
+			echo '<h1>'.$tituloViaje["titulo"].'</h1>';
+			echo $html;
+			echo '<h1>Precio: '.$precio.' €</h1>';
+			echo '</div>';
+			echo '<form method="post" action="combo.php?id='.$idcombo.'">';
+			echo '<div id="boton">';
+			echo '<input type="submit" value="Ver mas">';
+			echo '</div>';
+			echo '</form>';
+			echo '</div>';
 			}
 		}
-	}
 ?>
 
 <html>
