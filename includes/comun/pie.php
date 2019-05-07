98f8862c5076fdd18279f84f91706a4d49a4b758
samusolo <samusolo@ucm.es>
2019-05-07 17:01:51 +0200

    Add files via upload

--- includes/comun/pie.php
+++ includes/comun/pie.php
@@ -1,4 +1,4 @@
 <div id="pie">
 	<p>Trabajo realizado por estudiantes del grado en informática de la Universidad Complutense de Madrid.<p>
-	<p>Rubén Peña, Adrian Agudo, Arantxa Patricia Brock, Samuel Solo</p>
+	<p>Rubén Peña, Adrian Agudo, Arantxa Patricia Brock, Samuel Solo de Zaldívar</p>
 </div>
