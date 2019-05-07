e5513ad49e469c101346ed3dfda9f23068db0e29
aagudo01 <aagudo01@ucm.es>
2019-05-07 19:35:09 +0200

    EL UNICO QUE HA METIDO JAVASCRIPT

--- includes/comun/izquierda.php
+++ includes/comun/izquierda.php
@@ -1,6 +1,14 @@
 <?php
 	include (dirname(__DIR__)."/Ranking.php");
 ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script>
+	$(document).ready(function() {
+		$("#izquierda").mouseenter(function(){
+			alert("Este es un ranking en el que podemos observar a los usuarios cuyo contenido es el mejor puntuado de la pagina");
+		});
+	})
+</script>
 <div id="izquierda">
 	<?php 
 		$ranking = new Ranking();
