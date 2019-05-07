557b7000c1c2dd2ca222fad4d225ec30b526a493
samusolo <samusolo@ucm.es>
2019-05-07 17:50:54 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 				echo '<h1>'.$viaje["TITULO"].'</h1>';
 				echo '<p>'.$viaje["DESCB"].'<p>';
 				echo '<p>De '.$viaje["FECHAINI"].' a '.$viaje["FECHAFIN"]. '<p>';
-				echo '<p>Precio: '.$viaje["PRECIO"].'</p>';
+				echo '<p>Precio: '.$viaje["PRECIO"].' €</p>';
 				echo '</div>';
 				echo '<form method="post" action="viaje.php?id='.$valor.'">';
 				echo '<div id="boton">';
@@ -58,7 +58,7 @@
 				echo '<div id="info">';
 				echo '<h1>'.$actividad["TITULO"].'</h1>';
 				echo '<p>'.$actividad["DESCB"].'<p>';
-				echo '<p>Precio: '.$actividad["PRECIO"].'</p>';
+				echo '<p>Precio: '.$actividad["PRECIO"].' €</p>';
 				echo '</div>';
 				echo '<form method="post" action="actividad.php?id='.$valor.'">';
 				echo '<div id="boton">';
