77516cd8d233201cb48e7e7c48be4c88fc458ab0
rbn423 <33626150+rbn423@users.noreply.github.com>
2019-05-12 13:00:08 +0200

    Revert "Merge branch 'develop' of https://github.com/rbn423/chesterplans into develop"
    This reverts commit ee116d6d4882c91f4fa27e021b2d5a7155da1d3c, reversing
    changes made to 979fe8cf142e79599fcd803fa0b6be56f043dd6e.

--- includes/Selector.php
+++ includes/Selector.php
@@ -4,27 +4,9 @@
 	class Selector{
 
 		public static function genera($vista){
-
 			switch($vista){
 				case "viajes":
 				echo <<<EOF
-					<form method="post" action="buscador.php">
-						<div id = "selector">
-							<p>
-							Buscar en: <select name="tema">
-												<option value="todo">Todo</option>
-												<option value="viajes">Viajes</option>
-												<option value="actividades">Actividades</option>
-												<option value="experiencias">Experiencias</option>
-										<select>
-							</p>
-				<p><input type="textarea" name="buscar" placeholder="Buscador"</p>
-				<p>
-					<input type="submit" value="Enviar">
-				</p>
-				</div>
-				</form>
-						
 				<form method="post" action="vistaViajes.php">
 					<div id="selector">
 						<p>
@@ -69,22 +51,6 @@ EOF;
 break;
 			case "experiencias":
 			echo <<<EOF
-			<form method="post" action="buscador.php">
-						<div id = "selector">
-							<p>
-							Buscar en: <select name="tema">
-												<option value="todo">Todo</option>
-												<option value="viajes">Viajes</option>
-												<option value="actividades">Actividades</option>
-												<option value="experiencias">Experiencias</option>
-										<select>
-							</p>
-				<p><input type="textarea" name="buscar" placeholder="Buscador"</p>
-				<p>
-					<input type="submit" value="Enviar">
-				</p>
-				</div>
-				</form>
 				<form method="post" action="vistaExperiencias.php">
 					<div id="selector">
 						<p>Filtrar por:</p>
@@ -103,22 +69,6 @@ EOF;
 break;
 			case "actividades":
 			echo <<<EOF
-			<form method="post" action="buscador.php">
-						<div id = "selector">
-							<p>
-							Buscar en: <select name="tema">
-												<option value="todo">Todo</option>
-												<option value="viajes">Viajes</option>
-												<option value="actividades">Actividades</option>
-												<option value="experiencias">Experiencias</option>
-										<select>
-							</p>
-				<p><input type="textarea" name="buscar" placeholder="Buscador"</p>
-				<p>
-					<input type="submit" value="Enviar">
-				</p>
-				</div>
-				</form>
 				<form method="post" action="vistaActividades.php">
 					<div id="selector">
 						<p>
